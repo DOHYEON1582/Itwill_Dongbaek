f2e0c04ba682177bdd2272c82b9a2f6936fc9980
DOHYEON1582 <pondes@naver.com>
2024-04-12 14:26:11 +0900

    Create_Project

---
+++ DongbaekMarketProject/src/main/java/com/itwillbs/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.itwillbs.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public void main()throws Exception {
+		logger.debug(" main() 호출 ");
+
+
+	}
+	
+}
