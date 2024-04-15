cb8644b2cd210d28a6087b4c43918af2caf751b3
DOHYEON1582 <pondes@naver.com>
2024-04-15 11:48:07 +0900

    Test_commit2

---
+++ DongbaekMarketProject/src/test/java/com/itwillbs/controller/ProjectTest.java
@@ -0,0 +1,17 @@
+package com.itwillbs.controller;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class ProjectTest {
+
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProjectTest.class);
+	
+	
+}//endTest
