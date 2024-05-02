f8a9048cee888543354aa054346c1e4d1da5117f
0w0g <bonbon.riibon@gmail.com>
2024-05-02 12:42:33 +0900

    결제 추가, 수정필요

--- DongbaekMarketProject/src/main/java/com/itwillbs/service/OrderService.java
+++ DongbaekMarketProject/src/main/java/com/itwillbs/service/OrderService.java
@@ -7,10 +7,13 @@ import com.itwillbs.domain.OrderInfoVO;
 
 public interface OrderService {
 
-	// 장바구니 목록 가져오기
+	// 장바구니 목록 가져오기 (전체주문 할 때 사용)
 	public List<CartVO> selectCartList(CartVO vo) throws Exception;
+	
+	// 장바구니 상품 수 세어오기
+	public int selectCountCart(CartVO vo) throws Exception; 
 		
-	// 주문 할 상품 정보 가져오기
+	// 주문 할 상품 정보 가져오기 (선택 주문, 개별 주문 할 때 사용)
 	public CartVO selectProductInfo(int cart_code) throws Exception;
 	
 	// 적립금 가져오기
