101487e8e296e1319d56f58a03e6e82807a6cf5d
95hyunjin <guswls5126@naver.com>
2024-05-07 15:02:04 +0900

    20240507 1

--- DongbaekMarketProject/src/main/java/com/itwillbs/domain/UserVO.java
+++ DongbaekMarketProject/src/main/java/com/itwillbs/domain/UserVO.java
@@ -1,12 +1,12 @@
 package com.itwillbs.domain;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class UserVO {
+	
 	private String user_id;
 	private String user_pw;
 	private String salt;
