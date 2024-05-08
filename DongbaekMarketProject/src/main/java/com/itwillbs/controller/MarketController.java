f2acc12c52ebc8a74c738838ee5719b948460016
95hyunjin <guswls5126@naver.com>
2024-05-08 12:43:23 +0900

    20240508 - 문의 답글작성, 중복체크 완

--- DongbaekMarketProject/src/main/java/com/itwillbs/controller/MarketController.java
+++ DongbaekMarketProject/src/main/java/com/itwillbs/controller/MarketController.java
@@ -132,6 +132,7 @@ public class MarketController {
 				.contentType(MediaType.valueOf("text/plain; charset=UTF-8"))
 				.body("성공");
 	}
+	
 	@PostMapping("/checkDuplicateAnswer")
 	public ResponseEntity<String> checkDuplicateAnswer(@RequestParam("q_code") int q_code) throws Exception {
 	    logger.debug(" checkDuplicateAnswer() 실행 ");
