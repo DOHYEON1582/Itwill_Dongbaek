be9ef65df6fcbb8c4fab42714a1ea0595771c714
kjj <155524963+KJJ0513@users.noreply.github.com>
2024-05-14 09:46:49 +0900

    sdd

--- DongbaekMarketProject/src/main/java/com/itwillbs/persistence/OrderDAO.java
+++ DongbaekMarketProject/src/main/java/com/itwillbs/persistence/OrderDAO.java
@@ -15,7 +15,7 @@ public interface OrderDAO {
 	public int selectCountCart(CartVO vo) throws Exception; 
 		
 	// 주문 할 상품 정보 가져오기
-	public CartVO selectProductInfo(int cart_code) throws Exception;
+	public CartVO selectProductInfo(int cart_code) throws Exception; 
 	
 	// 적립금 가져오기
 	public String selectUserPoint(String user_id) throws Exception;
